d4a17000240b4d4a13f912ac5674243d952256fc
juanjose <juanjoperez2121@gmail.com>
2024-07-10 11:20:27 -0500

    se sube otra vez

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Header } from './components/Header'
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [gifs, setGifs] = useState([]);
+
+  const handleSearch = async () => {
+    const API_KEY = 'pWyTBl9Twn7NM2jANxpEf2RO08KZwWnA';  
+    const URL = `"https://api.giphy.com/v1/gifs/search"`;
+
+    try {
+      const response = await axios.get(URL);
+      setGifs(response.data.data);
+    } catch (error) {
+      console.error('Error fetching data from Giphy API:', error);
+    }
+  };
 
   return (
-    <>
-     <Header/>
-    </>
-  )
-}
+    <div className="container">
+      <h1 className="title">Giphy Search</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Buscar GIFs"
+        />
+        <button onClick={handleSearch}>Buscar</button>
+      </div>
+      <div className="gifs-container">
+        {gifs.map((gif) => (
+          <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
