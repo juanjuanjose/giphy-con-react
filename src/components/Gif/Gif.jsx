046024d24a56d63334d1a15921f14f273a251fab
juanjose <juanjoperez2121@gmail.com>
2024-07-10 08:27:58 -0500

    se sube

---
+++ src/components/Gif/Gif.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Gif() {
+    const apikey = "ZTFRE42dTEv4S5htqryQnRa2OZ2qyoAu"
+    const palabrasAleatorias = ["random", "funny" , "cute", "nature","animals", "sports", "gaming", "music", "movies"];
+    const query = palabrasAleatorias[Math.floor(Math.random() * palabrasAleatorias.length)];
+
+  return (
+    <></>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Header.css'
+export const Header = () => {
+  return (
+    <>
+    <header><h1>Pagina de gifs</h1></header> 
+    </>
+  )
+}
